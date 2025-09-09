2a317c9341bac52b1337dec2f4d5484a33621b43
itdavidivan <149909071+itdavidivan@users.noreply.github.com>
2025-09-09 11:57:39 +0200

    register endpoint, added cors

--- main.ts
+++ main.ts
@@ -1,14 +1,29 @@
 import express from "express";
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { usersTable } from "./lib/drizzle/schema.ts";
+import cors from "cors";
+
 const db = drizzle(process.env.DATABASE_URL!);
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-app.post("/auth/register", (req, res) => {
-  res.send("Register endpoint");
+app.post("/auth/register", async (req, res) => {
+  try {
+    await db
+      .insert(usersTable)
+      .values({ username: req.body.username, password: req.body.password });
+
+    res.send("Register endpoint");
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.listen(8080, () => {
